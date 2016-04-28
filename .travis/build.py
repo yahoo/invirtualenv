5773017f6c762f9ea9b52fbc8d960cf45c2d4128
Dwight Hubbard <dhubbard@yahoo-inc.com>
2016-04-28 15:31:19 -0700

    Move the travis commands to it's own script

---
+++ .travis/build.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import os
+import subprocess
+import sys
+
+version = os.environ.get('TRAVIS_PYTHON_VERSION', '2.7')
+print('PYTHON_VERSION: ' + version)
+
+test_env = version
+if version not in ['pypy', 'pypy3']:
+  test_env = 'py' + version.replace('.', '')
+
+command = ['tox', '-e', 'test_env']
+try:
+  subprocess.check_output(command)
+  sys.exit(0)
+except subprocess.CalledProcessError as commandfail:
+  print('Tox failed with return code: %d' % commandfail.returncode)
+  sys.exit(commandfail.returncode)
+sys.exit(0)
