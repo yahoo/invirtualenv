db4d10adf7bd116a5d4a224b0deab0a4f82f7353
dhubbard <dhubbard@oath.com>
2018-08-24 17:16:29 -0700

    Prevent the parsedconfig plugin from keeping list_plugins from working when there is no deploy.conf file present.

--- invirtualenv_plugins/parsedconfig.py
+++ invirtualenv_plugins/parsedconfig.py
@@ -10,21 +10,19 @@ logger = logging.getLogger(__name__)
 class InvirtualenvParsedConfig(InvirtualenvPlugin):
     package_formats = ['parsed_deploy_conf']
     default_config_filename = 'deploy.conf.parsed'
+    package_template = None
 
     def __init__(self, *args, **kwargs):
         super(InvirtualenvParsedConfig, self).__init__(*args, **kwargs)
-        if not os.path.exists(self.config_file):
-            raise FileNotFoundError('Configuration file %r was not found' % self.config_file)
 
-        with open(self.config_file) as config_file_handle:
-            self.package_template = config_file_handle.read()
-        logger.debug('Read template %r', self.package_template)
+        if os.path.exists(self.config_file):
+            with open(self.config_file) as config_file_handle:
+                self.package_template = config_file_handle.read()
+            logger.debug('Read template %r', self.package_template)
 
     def run_package_command(self, package_hashes, wheel_dir='wheels'):
-        logger.debug('Config')
-        logger.debug(self.config)
-        logger.debug('Deploy.conf')
-        logger.debug(open('deploy.conf').read())
+        if not os.path.exists(self.config):
+            raise FileNotFoundError('The invirtualenv configuration file %r was not found' % self.config)
         generate_parsed_config_file('deploy.conf', self.default_config_filename)
         return self.default_config_filename
 
