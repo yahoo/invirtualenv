00f5501a74e7cd3016bd44b74ea72c136aaa9e9e
Dwight Hubbard <254983+dwighthubbard@users.noreply.github.com>
2018-08-27 14:38:41 -0700

    Update version (#9)
    * Update the docker plugin to accept most valid dockerfile options.
    
    Fix lint/test issues in order to have my PR build
    
    Disable debug logging in the cli
    
    * Add more tests
    
    * Add more tests

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import json
 from setuptools import setup
 
 # Default version number
-BASE_VERSION = '1.4.1'
+BASE_VERSION = '18.8.0'
 METADATA_FILENAME = 'invirtualenv/package_metadata.json'
 BASEPATH = os.path.dirname(os.path.abspath(__file__))
 METADATA_FILE = os.path.join(BASEPATH, METADATA_FILENAME)
