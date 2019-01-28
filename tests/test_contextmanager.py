431272cd2f2c2f078d9ef479752cfbab3f767e03
dhubbard <dhubbard@oath.com>
2019-01-28 15:09:16 -0800

    Add more tests to address coverage drops.

---
+++ tests/test_contextmanager.py
@@ -0,0 +1,15 @@
+import unittest
+import invirtualenv.contextmanager
+
+
+class TestContextmanager(unittest.TestCase):
+    def test__revert_file(self):
+        with invirtualenv.contextmanager.InTemporaryDirectory():
+            with open('testfile', 'w') as fh:
+                fh.write('original')
+            self.assertEqual('original', open('testfile').read())
+            with invirtualenv.contextmanager.revert_file('testfile'):
+                with open('testfile', 'w') as fh:
+                    fh.write('changed')
+                self.assertEqual('changed', open('testfile').read())
+            self.assertEqual('original', open('testfile').read())
