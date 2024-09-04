6e437349466f7551cb60fb46d5c3b11e20eedfce
Dwight Hubbard <dhubbard@yahooinc.com>
2024-09-04 15:58:42 -0700

    Remove the EOL CentOS 7.x verification.

--- invirtualenv/virtualenv.py
+++ invirtualenv/virtualenv.py
@@ -181,13 +181,15 @@ def build_virtualenv(
 
     virtualenv_dir = os.path.join(directory, name)
 
-    if BUILTIN_VENV and not hasattr(sys, 'frozen'):
+    try:
         logger.debug(
-            'Building virtualenv %r using the built in venv module',
+            'Attempting to build virtualenv %r using the built in venv module',
             virtualenv_dir
         )
+        import venv
         venv.create(virtualenv_dir, with_pip=True)
-    else:
+    except ImportError:
+        logger.warning('Unable to create virtualenv using the venv module, falling back to the virtualenv package')
         logger.debug('Building virtualenv using the virtualenv package, BUILTIN_VENV = BUILTIN_VENV')
         os.chdir(directory)
         command = [virtualenv_command()]
